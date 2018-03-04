b7d4b53df6f3245b72cd2ba7b1cf0e64b511dd62
Kyle <brickatyourfeet@gmail.com>
2018-03-04 21:49:41 +0000

    add form for stock

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
   def index
+    if params[:id] == ""
+      @empty = "Enter a ticket symbol"
+    # elsif
+    
+      # if params[:id]
+      #   @stock = #fetch here
+      # end
+    end  
+    
+    
   end
   
   def about
